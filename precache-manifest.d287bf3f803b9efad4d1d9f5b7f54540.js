084d53172596d261ee105dee46d6ea19ff9bfb5a
Sergey Zelinsky <zelinsky.serega@gmail.com>
2020-02-11 17:05:51 +0200

    Updates

---
+++ precache-manifest.d287bf3f803b9efad4d1d9f5b7f54540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62220a79e46b75f8840db7f899ffc48a",
+    "url": "/goit-react-image-finder/index.html"
+  },
+  {
+    "revision": "2a70995bc781fcf7d2d0",
+    "url": "/goit-react-image-finder/static/css/main.e58979ef.chunk.css"
+  },
+  {
+    "revision": "2a5b770abca87a607f96",
+    "url": "/goit-react-image-finder/static/js/2.b8f86f04.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-image-finder/static/js/2.b8f86f04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a70995bc781fcf7d2d0",
+    "url": "/goit-react-image-finder/static/js/main.b611ab85.chunk.js"
+  },
+  {
+    "revision": "e245377d1fb92d10c848",
+    "url": "/goit-react-image-finder/static/js/runtime-main.2920a787.js"
+  }
+]);
